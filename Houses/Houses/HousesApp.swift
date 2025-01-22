2a5009167134c5bdde903bbed33109048844ba0e
Jay Dethariya <jaydethariya4work@gmail.com>
2025-01-22 18:03:26 +0530

    Initial Commit

---
+++ Houses/Houses/HousesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HousesApp.swift
+//  Houses
+//
+//  Created by Vishal Kundaliya on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HousesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HouseView()
+        }
+    }
+}
